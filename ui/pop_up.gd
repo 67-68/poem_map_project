b5fd5d4bdcb77802410d53d2aa9d905a79759a1a
6768 <amongus6768@icloud.com>
2026-02-05 17:06:48 +0800

    popup view test

--- ui/pop_up.gd
+++ ui/pop_up.gd
@@ -1,15 +1,13 @@
 extends PanelContainer
 	
 func on_text_popup(text: String):
-	$MarginContainer/TextLabel.text = text
+	$Con/TextLabel.text = text
 	show()
 	await get_tree().create_timer(3.0).timeout.connect(hide)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.request_text_popup.connect(on_text_popup)
-	$MarginContainer/TextLabel.modulate = Color.RED
-	
+	Global.request_text_popup.connect(on_text_popup)	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- view_tests/vtest_popup_notification.gd
+++ view_tests/vtest_popup_notification.gd
@@ -1,4 +1,4 @@
-class_name VTestPopupNotification extends RefCounted
+class_name TestPopupNotification extends RefCounted
 
 func create_debug_view(idx: int):
 	var view_scene := preload('res://ui/pop_up.tscn')
@@ -11,13 +11,16 @@ func create_debug_view(idx: int):
 			return null
 	
 	# 务实建议：显式指定大小，防止 HFlowContainer 排版塌陷 💀
-	view.custom_minimum_size = Vector2(100, 100)
+	view.custom_minimum_size = Vector2(300, 100)
 	return view
 
 func test_action_func(map: Dictionary):
-	var cur_view = map.get('0').get_node('TextLabel')
+	var cur_view = map.get('0').get_node('Con/TextLabel')
 	if cur_view and cur_view.is_node_ready():
-		cur_view.text = Util.colorize_underlined_link('李白',Color.GRAY,'')
+		var poet = Util.colorize_underlined_link('李白',Color.GRAY,'')
+		var period = Util.colorize_underlined_link('唐朝',Color.GOLD,'')
+		var time = Util.colorize_underlined_link('705-755',Color.PERU,'')
+		cur_view.text = poet + '是一名' + period + ' ' + time + '诗人'
 
 func get_actions() -> Array[ViewTestAction]:
 	return [
