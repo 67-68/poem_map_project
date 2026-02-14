edd7f8a9a1fd4490f041a55b6db0094d42ac3a66
6768 <amongus6768@icloud.com>
2026-02-14 15:17:32 +0800

    done final debug

--- characters/messager.gd
+++ characters/messager.gd
@@ -45,8 +45,8 @@ func start_travel():
 
 func start_timer():
 	if allow_timer:
-		breakpoint
-		TextPoolManager.spawn(txt,global_position)
+		#breakpoint
+		TextPoolManager.spawn(txt,$MsgPathFollow.global_position)
 		print($MsgPathFollow.global_position)
 		var timer = get_tree().create_timer(randi() % 5)
 		timer.timeout.connect(start_timer)
