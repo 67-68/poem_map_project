0a2877e37359a28923798b3196c4ebf8fb7c6fe2
6768 <amongus6768@icloud.com>
2026-03-29 16:00:29 +0800

    - poem trait uuid change - the 6th main action event

--- model/event.gd
+++ model/event.gd
@@ -1,5 +1,5 @@
 class_name BaseEvent extends GameEntity
-@export var options: Array[BaseOption] = []
+@export var options: Array[BaseOption] = [EventOption.new(),EventOption.new()]
 @export var example: String
 @export var requirement: BaseRequirements
 @export var audio: AudioStream = null
