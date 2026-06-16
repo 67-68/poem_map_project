a786a9cc481b2aacab14786016447ae464994e44
6768 <amongus6768@icloud.com>
2026-06-16 20:51:46 +0800

    创建数据

--- core/csv_cloud_loader.gd
+++ core/csv_cloud_loader.gd
@@ -133,6 +133,12 @@ const DATA_MANIFEST: Array[Dictionary] = [
         "data_type": "random_event",
         "is_generated": true,
     },
+    {
+        "name": "清流钻营事件库",
+        "save_path": "res://data/4_eras/747_kuangda/_qingliu_zuanying_events.csv",
+        "data_type": "random_event",
+        "is_generated": true,
+    },
 ]
 
 # 🗺️ store_to 路径映射表
